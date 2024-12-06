154c17d8b7671168d17d9a16dc6135708945edca
LoganathanGitHub <121919378+LoganathanGitHub@users.noreply.github.com>
2024-12-06 14:14:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("first js");
+console.log("JS file in link");
+//alert("link Alert");
+// Variable Declaration
+let courseName2;
+
+let courseName="Front-End-Developer",
+contantNo="68768768",
+duraTion="One Month",
+classMode="Offline";
+
+console.log(courseName,contantNo,duraTion,classMode);
+
+const name="logu";
+
+
